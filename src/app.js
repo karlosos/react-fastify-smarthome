74ec7888e35367a18ed272638a20afe1de246088
makeros <mail@arekczogala.pl>
2020-02-24 08:09:59 +0100

    chore: project setup

---
+++ src/app.js
@@ -0,0 +1,34 @@
+module.exports = function ({ port }) {
+  const app = require('fastify')({ logger: true })
+
+  app.register(require('fastify-swagger'), {
+    routePrefix: '/.well-known/documentation',
+    swagger: {
+      info: {
+        title: 'Patronage 2020 JS',
+        description: '',
+        version: '0.1.0'
+      },
+      externalDocs: {
+        url: 'https://swagger.io',
+        description: 'Find more info here'
+      },
+      host: 'localhost' + ':' + port,
+      schemes: ['http'],
+      consumes: ['application/json'],
+      produces: ['application/json']
+    },
+    exposeRoute: true
+  })
+  // Declare a route
+  app.get('/.well-known/health-check', async (request, reply) => {
+    reply.send('OK')
+  })
+
+  app.ready(err => {
+    if (err) throw err
+    app.swagger()
+  })
+
+  return app
+}
