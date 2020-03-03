b275aa4f088a08eef5f9b9f1ec81e072ee293f33
makeros <mail@arekczogala.pl>
2020-03-03 14:51:44 +0100

    chore: routes nesting example

--- src/app.js
+++ src/app.js
@@ -6,36 +6,38 @@ module.exports = function ({ port }) {
 
   app.use('/', serveStatic(path.join(__dirname, '..', 'frontend', 'dist')))
 
-  app.register(require('fastify-swagger'), {
-    routePrefix: '/.well-known/documentation',
-    swagger: {
-      info: {
-        title: 'Patronage 2020 JS',
-        description: '',
-        version: '0.1.0'
-      },
-      externalDocs: {
-        url: 'https://swagger.io',
-        description: 'Find more info here'
-      },
-      host: 'localhost' + ':' + port,
-      schemes: ['http'],
-      consumes: ['application/json'],
-      produces: ['application/json']
-    },
-    exposeRoute: true
-  })
+  app
+    .register((instance, opts, next) => {
+      instance.register(require('fastify-swagger'), {
+        routePrefix: '/documentation',
+        swagger: {
+          info: {
+            title: 'Patronage 2020 JS',
+            description: '',
+            version: '0.1.0'
+          },
+          externalDocs: {
+            url: 'https://swagger.io',
+            description: 'Find more info here'
+          },
+          host: 'localhost' + ':' + port,
+          schemes: ['http'],
+          consumes: ['application/json'],
+          produces: ['application/json']
+        },
+        exposeRoute: true
+      })
+        .ready(err => {
+          if (err) throw err
+          instance.swagger()
+        })
 
-  app.get('/.well-known/health-check', async (request, reply) => {
-    reply.send({
-      message: 'OK'
+      instance.register(require('./routes/well-known/health-check'))
+
+      next()
+    }, {
+      prefix: '.well-known'
     })
-  })
-
-  app.ready(err => {
-    if (err) throw err
-    app.swagger()
-  })
 
   return app
 }
