5504092137374df3ec0853fd6507ce04c061e676
MartaJaszewska <jaszewska.marta@gmail.com>
2020-03-06 20:55:13 +0100

    fix:[61] add test, changed status code to 404 in get endpoint, fixed nodemon script

--- src/routes/authors.js
+++ src/routes/authors.js
@@ -1,15 +1,12 @@
-const { authors } = require('../public/authors.json');
+const authors = require('../public/authors.json');
 
 const routes = async (fastify, options) => {
   fastify.get('/', (request, reply) => {
     reply.send(authors);
   });
 
-  fastify.get('/1', async (request, reply) => {
-    const author = await authors.find(author => author.id === 1);
-    author
-      ? reply.send(author)
-      : reply.status(400).send('Server cannot process the request.');
+  fastify.get('/1', (request, reply) => {
+    reply.send(authors[0]);
   });
 };
 
--- tests-server/authors.spec.js
+++ tests-server/authors.spec.js
@@ -1,6 +1,6 @@
 /* globals beforeAll, afterAll, describe, test, expect */
 const app = require('./../src/app.js');
-const { authors } = require('../src/public/authors.json');
+const authors = require('../src/public/authors.json');
 // Start application before running the test case
 describe('/api/v1/authors', function() {
   let instance;
@@ -30,9 +30,18 @@ describe('/api/v1/authors', function() {
         url: '/api/v1/authors/1'
       });
       expect(result.statusCode).toBe(200);
-      expect(JSON.parse(result.payload)).toEqual(
-        authors.find(author => author.id === 1)
-      );
+      expect(JSON.parse(result.payload).id).toBeDefined();
+      expect(JSON.parse(result.payload)).toEqual(authors[0]);
+    });
+
+    test('Should return status code 404 and undefined id', async function() {
+      const notExistingAuthorId = authors.length + 1;
+      const result = await instance.inject({
+        method: 'GET',
+        url: `/api/v1/authors/${notExistingAuthorId}`
+      });
+      expect(result.statusCode).toBe(404);
+      expect(JSON.parse(result.payload).id).toBe(undefined);
     });
   });
 });
