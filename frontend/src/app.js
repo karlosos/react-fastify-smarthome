a6d1ead2e0aa293955a1dc9e06f3e7ea21f0b56a
Jakub Wieckowski <kubaw1@onet.eu>
2020-03-18 20:59:05 +0100

    chore: PATRON2020-105 change routes handling, add test, modify List styles

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -5,15 +5,20 @@ import AuthorsList from '@views/AuthorsList.jsx'
 import Layout from '@views/Layout.jsx'
 import Dashboard from '@components/Layout/Dashboard'
 
-const App = () => (
-  <Layout>
+const App = () => {
+  const routes = (
     <Switch>
       <Route path='/authors/:id' component={AuthorPage} />
       <Route path='/authors' component={AuthorsList} />
+      <Route path='/hvac' render={() => 'hvac'} />
       <Route path='/' exact component={Dashboard} />
       <Redirect to='/' />
     </Switch>
-  </Layout>
-)
-
+  )
+  return (
+    <Layout>
+      {routes}
+    </Layout>
+  )
+}
 export default App
