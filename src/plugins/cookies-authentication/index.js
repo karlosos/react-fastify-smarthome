82bb03bb56dbfac35debd65b65568bceef5b9dd5
Karol Dzialowski <karlososhd@gmail.com>
2020-04-02 20:52:57 +0200

    feat: [PATRON2020-136] add cookies authentication

---
+++ src/plugins/cookies-authentication/index.js
@@ -0,0 +1,23 @@
+const fp = require('fastify-plugin')
+
+module.exports = fp(function (fastify, options, next) {
+  fastify.register(require('fastify-cookie'), {
+    parseOptions: {}
+  })
+
+  fastify.addHook('onRequest', (request, reply, done) => {
+    const serverSecret = fastify.config.COOKIE_SECRET
+    if (serverSecret !== '') {
+      const requestSecret = request.cookies.secret
+      if (serverSecret !== requestSecret) {
+        reply.code(401)
+        reply.send({
+          error: 'Unauthorized Error'
+        })
+      }
+    }
+    done()
+  })
+
+  next()
+})
---
+++ src/plugins/env/index.js
@@ -0,0 +1,34 @@
+const fastifyEnv = require('fastify-env')
+const fp = require('fastify-plugin')
+
+module.exports = fp(function (fastify, options, next) {
+  const schema = {
+    type: 'object',
+    required: ['COOKIE_SECRET'],
+    properties: {
+      COOKIE_SECRET: {
+        type: 'string',
+        default: ''
+      }
+    }
+  }
+
+  const envOptions = {
+    confKey: 'config',
+    schema: schema,
+    dotenv: {
+      path: './.env',
+      debug: true
+    }
+  }
+
+  fastify
+    .register(fastifyEnv, envOptions)
+    .ready((err) => {
+      if (err) console.error(err)
+
+      console.log(fastify.config)
+    })
+
+  next()
+})
