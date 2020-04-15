79f2cf7eaa288b0e02e5dffe00d0894cf9082384
Jakub Wieckowski <kubaw1@onet.eu>
2020-04-15 21:44:20 +0200

    fix: PATRON2020-97 add context in sensor test

--- frontend/src/components/Dashboard/SmartHomeMap/Map/Sensor/Sensor.test.jsx
+++ frontend/src/components/Dashboard/SmartHomeMap/Map/Sensor/Sensor.test.jsx
@@ -3,15 +3,46 @@ import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import '@testing-library/jest-dom/matchers'
 import Sensor from './Sensor'
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
 
 const styleMock = {
   top: '20px', left: '50px'
 }
 
+const mockedSensorState = {
+  sensors: [],
+  loadingSensors: false,
+  loadingError: null,
+  sensorError: null
+}
+
+const mockedMapState = {
+  listItemPressed: false,
+  pressedItemId: undefined,
+  waitingForSensorLocation: false,
+  sensorColor: undefined,
+  sensorData: undefined
+}
+
+const mockedDbInteractionState = {
+  _id: 0,
+  type: 'None',
+  mapPosition: undefined,
+  addingPoint: false,
+  dbError: undefined
+}
+
 describe('Sensor component test suite', () => {
+  const initialState = { sensor: mockedSensorState, mapListCommunication: mockedMapState, dbInteraction: mockedDbInteractionState }
+  const mockStore = configureStore()
+  const store = mockStore(initialState)
+
   test('should have styles', () => {
     expect(render(
-      <Sensor position={styleMock} />
+      <Provider store={store}>
+        <Sensor position={styleMock} />
+      </Provider>
     ).getByTestId('sensor-id')).toHaveStyle(styleMock)
   })
 })
