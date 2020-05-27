e8f723600aa78e66f06ccc8d1f9527f923ca791a
MartaJaszewska <jaszewska.marta@gmail.com>
2020-05-27 12:38:30 +0200

    fix: [PATRON2020-449] restore functionality of updating notification

--- frontend/src/data/sagas/notification/index.js
+++ frontend/src/data/sagas/notification/index.js
@@ -1,11 +1,11 @@
 import { takeLatest } from 'redux-saga/effects'
 import actionTypes from '@constants/actionTypes'
 import {
-  fetchNotificationsSaga, checkNotificationsSaga
+  fetchNotificationsSaga, checkNotificationsSaga, updateNotificationsSaga
 } from './notificationSagas'
 
 export function * watchNotifications () {
   yield takeLatest(actionTypes.NOTIFICATIONS_FETCH_REQUEST, fetchNotificationsSaga)
-  yield takeLatest(actionTypes.NOTIFICATIONS_UPDATE, fetchNotificationsSaga)
+  yield takeLatest(actionTypes.NOTIFICATIONS_UPDATE, updateNotificationsSaga)
   yield takeLatest(actionTypes.NOTIFICATIONS_CHECK, checkNotificationsSaga)
 }
