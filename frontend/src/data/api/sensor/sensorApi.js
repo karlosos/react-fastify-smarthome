a2359956baf179cf425f2639f805d04d67bda278
MartaJaszewska <jaszewska.marta@gmail.com>
2020-06-06 12:57:08 +0200

    refactor: clean code

--- frontend/src/data/api/sensor/sensorApi.js
+++ frontend/src/data/api/sensor/sensorApi.js
@@ -1,18 +1,9 @@
 import axios from 'axios'
-import { delay } from 'redux-saga/effects'
 
 export function * getSensors () {
   const res = yield axios.get('/api/v1/dashboard')
 
   return res.data
-}
-
-export function * changeSensorStatus (sensorId) {
-  // const res = yield axios.put(`/on-off/endpoint/${sensorId}`)
-  // return res.data
-
-  yield delay(1000) // mock async, should be removed once real async requests are made
-  return 'mock response'
 }
 
 export function * refreshSensors () {
